52849dab97245720d448aa979ccf29b490561db6
0xRomRom <roman0811@live.nl>
2024-11-27 01:11:26 +0100

    queue added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import stl from "./App.module.css";
 import { tokensArray } from "./tokens";
 import MainList from "./components/mainlist/MainList";
+import Queue from "./components/queue/Queue";
 
 const App = () => {
   const [tokens, setTokens] = useState(tokensArray);
+  const [queueTokens, setQueuedTokens] = useState([]);
 
   return (
     <div className={stl.app}>
       <MainList tokens={tokens} setTokens={setTokens} />
+      <Queue queueTokens={queueTokens} />
     </div>
   );
 };
---
+++ src/components/queue/Queue.jsx
@@ -0,0 +1,25 @@
+import stl from "./Queue.module.css";
+import { GoInbox } from "react-icons/go";
+
+const Queue = ({ queueTokens }) => {
+  return (
+    <div className={stl.queue}>
+      <h2>Queue</h2>
+
+      <ul>
+        {queueTokens.length === 0 && (
+          <div className={stl.emptyQueue}>
+            <GoInbox />
+            Queue Empty
+          </div>
+        )}
+        {queueTokens.length > 0 &&
+          queueTokens.map((token) => {
+            return <li></li>;
+          })}
+      </ul>
+    </div>
+  );
+};
+
+export default Queue;
