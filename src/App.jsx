82035d6233e3d378cc5f9ad8f04ffe69f436c061
0xRomRom <roman0811@live.nl>
2024-11-27 02:18:27 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import { tokensArray } from "./tokens";
 import MainList from "./components/mainlist/MainList";
 import Queue from "./components/queue/Queue";
 import Config from "./components/config/Config";
+import Logs from "./components/logs/Logs";
 
 const App = () => {
   const [tokens, setTokens] = useState(tokensArray);
   const [queueTokens, setQueuedTokens] = useState([]);
+  const [tradeLogs, setTradeLogs] = useState([]);
 
   return (
     <div className={stl.app}>
       <Config />
       <MainList tokens={tokens} setTokens={setTokens} />
       <Queue queueTokens={queueTokens} setQueuedTokens={setQueuedTokens} />
+      <Logs tradeLogs={tradeLogs} setTradeLogs={setTradeLogs} />
     </div>
   );
 };
